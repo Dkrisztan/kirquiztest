bfbdb16c85d8c3a1321fe904b3328f2273075272
Dkrisztan <krisztidancs@gmail.com>
2024-02-13 18:09:18 +0100

    render try

--- Kir-Quiz-test/src/question/question.controller.ts
+++ Kir-Quiz-test/src/question/question.controller.ts
@@ -4,10 +4,16 @@ import {
   Delete,
   Get,
   Param,
+  ParseFilePipe,
   Patch,
   Post,
   Query,
+  UploadedFile,
+  UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
+import { DeleteFileExceptionFilter } from 'src/util/DeleteFileExceptionFilter';
+import { IconInterceptor, IconValidators } from 'src/util/iconHelper';
 import { CreateQuestionDto } from './dto/create-question.dto';
 import { UpdateQuestionDto } from './dto/update-question.dto';
 import { QuestionService } from './question.service';
@@ -17,8 +23,19 @@ export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
   @Post()
-  create(@Body() createQuestionDto: CreateQuestionDto) {
-    return this.questionService.create(createQuestionDto, 'valami');
+  @UseInterceptors(IconInterceptor)
+  @UseFilters(DeleteFileExceptionFilter)
+  create(
+    @Body() createQuestionDto: CreateQuestionDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: IconValidators,
+        fileIsRequired: false,
+      }),
+    )
+    file?: Express.Multer.File,
+  ) {
+    return this.questionService.create(createQuestionDto, file?.filename);
   }
 
   @Get()
@@ -37,11 +54,20 @@ export class QuestionController {
   }
 
   @Patch(':id')
+  @UseInterceptors(IconInterceptor)
+  @UseFilters(DeleteFileExceptionFilter)
   update(
     @Param('id') id: string,
     @Body() updateQuestionDto: UpdateQuestionDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: IconValidators,
+        fileIsRequired: false,
+      }),
+    )
+    file?: Express.Multer.File,
   ) {
-    return this.questionService.update(id, updateQuestionDto, 'valami');
+    return this.questionService.update(id, updateQuestionDto, file?.filename);
   }
 
   @Delete(':id')
